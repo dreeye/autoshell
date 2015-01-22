2c1bd8fa71fc1a1efb92165be109420401077ff6
willis <willis@pronoz.(none)>
2015-01-22 14:29:40 +0800

    add common.sh python.sh

---
+++ common.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+# Check if user is root
+if [ $(id -u) != "0" ]; then
+    echo "Error: You must be root to run this script, please use root to install lnmp"
+    exit 1
+fi
+
+#show CentOS版本
+cat /etc/issue
+#show Linux core
+uname -a
+#show memory
+MemTotal=`free -m | grep Mem | awk '{print  $2}'`  
+echo -e "\n Memory is: $MemTotal MB "
+#Set timezone
+rm -rf /etc/localtime
+ln -s /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+
+yum install -y ntp
+ntpdate -u pool.ntp.org
+date
+
+alias_color=$(grep 'alias grep='\''grep --color=auto'\''' /etc/bashrc)
+if [ "$alias_color" == "" ]; then
+    sed -i '$a alias grep='\''grep --color=auto'\''' /etc/bashrc
+fi
+
+alias_utime=$(grep 'alias utime='\''sudo ntpdate us.pool.ntp.org'\''' /etc/bashrc)
+if [ "$alias_utime" == "" ]; then
+    sed -i '$a alias utime='\''sudo ntpdate us.pool.ntp.org'\''' /etc/bashrc
+fi
+
+#Disable SeLinux
+if [ -s /etc/selinux/config ]; then
+sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+fi
+
+for packages in gcc gcc-c++ autoconf libjpeg libjpeg-devel libpng libpng-devel freetype freetype-devel libxml2 libxml2-devel libxslt-devel libffi-devel zlib zlib-devel glibc glibc-devel glib2 glib2-devel bzip2 bzip2-devel ncurses ncurses-devel curl curl-devel e2fsprogs e2fsprogs-devel krb5 krb5-devel libidn libidn-devel openssl openssl-devel openldap openldap-devel nss_ldap openldap-clients openldap-servers zip unzip man perl-CPAN cmake bison wget mlocate git openssh-server openssh-clients patch make gcc-g77 flex file libtool libtool-libs kernel-devel libpng10 libpng10-devel gd gd-devel fonts-chinese gettext gettext-devel gmp-devel pspell-devel libcap diffutils libpcap-devel;
+do yum -y install $packages; done
